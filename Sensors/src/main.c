74acc6b01a3bebf3398d7d0d259e66fff5fb3457
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-10-15 11:49:48 +0300

    Add initial projects structure

---
+++ Sensors/src/main.c
@@ -0,0 +1,15 @@
+/**
+ *  @file main.c
+ *  @author Cristian Cristea
+ *  @date 15 October 2023
+ *
+ *  @brief Main source file for the Sensors station
+ **/
+
+
+#include <stdbool.h>
+
+void main(void)
+{
+    while (true) { }
+}
